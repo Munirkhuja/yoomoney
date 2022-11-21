d052bd95d603b10ef77280924d8d7e3e95908da9
munirhuja1357@gmail.com <135711_git>
2022-11-21 23:59:47 +0500

    first

--- app/Http/Controllers/Api/v1/Payment/YooMoneyController.php
+++ app/Http/Controllers/Api/v1/Payment/YooMoneyController.php
@@ -29,7 +29,7 @@ class YooMoneyController extends Controller
 
     public function payment_check(Request $request)
     {
-        $paid = YooMoney::check_payment($request->uniq_id);
+        $paid = YooMoney::check_payment($request->id);
         return view('paid')->with(['paid' => $paid ? 'paid' : 'not paid']);
     }
 }
