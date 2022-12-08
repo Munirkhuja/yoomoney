ec279115fb185f2960fadb175b518ceed2ec6a95
munirhuja1357@gmail.com <135711_git>
2022-12-08 19:43:16 +0500

    data train

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 //Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
-Route::get('/dataset/{dataset_id}', [SmartDevController::class, 'send_dataset']);
+Route::get('dataset/{dataset_id}', [SmartDevController::class, 'send_dataset']);
 Route::group(['prefix' => 'payments'], function () {
     Route::group(['prefix' => 'yoo-money'], function () {
         Route::post('/{user_id?}', YooMoneyController::class)->name('payment.yoo_money');
