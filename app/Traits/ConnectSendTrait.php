e44d6afbcf105269fd454dd7e4730a45d46307ac
munirhuja1357@gmail.com <135711_git>
2022-12-08 20:07:11 +0500

    data train

--- app/Traits/ConnectSendTrait.php
+++ app/Traits/ConnectSendTrait.php
@@ -26,14 +26,11 @@ trait ConnectSendTrait
         try {
             $response = $http->request($method, $url, $data);
             $response = json_decode((string)$response->getBody(), true);
-            if (500 == $response->getStatusCode() && $url != '/WebMarker/login' && $max_feed > 0) {
-                $mar = new MarkerApi();
-                $mar->Login();
-                $max_feed--;
-                $this->send($method, $url, $data, $max_feed);
-            }else{
-                return $response;
-            }
+            Log::build([
+                'driver' => 'single',
+                'path' => storage_path('logs/marker_api_con.log'),
+            ])->error((string)$response->getStatusCode() . ';' . $response);
+            return $response;
         } catch (\GuzzleHttp\Exception\ConnectException $e) {
             if (500 == $e->getResponse()->getStatusCode() && $url != '/WebMarker/login' && $max_feed > 0) {
                 $mar = new MarkerApi();
